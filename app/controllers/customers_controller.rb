c860113ebb7e7ec505a49b7767cb306e91c99870
MR.Y <programming@yatsukouji.com>
2015-11-23 07:26:10 +0900

    validationの実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,8 +9,11 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-    @customer.save
-    redirect_to @customer
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def edit
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,9 @@ class Customer < ActiveRecord::Base
     full_name = family_name + given_name + sama
     return full_name
   end
+  validates :family_name, presence: true, length: { maximum: 20 }
+  validates :given_name,  presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,       presence: true, format: { with: VALID_EMAIL_REGEX }
 end
 
