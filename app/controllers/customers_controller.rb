0a47ea1b84fd5fe955f1a4aee50481aca7ec5fb8
MR.Y <programming@yatsukouji.com>
2015-11-23 07:32:48 +0900

    edit,updateの実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -17,9 +17,16 @@ class CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
