fe905e39b8ede58b617aef09de12aede5089631e
MR.Y <programming@yatsukouji.com>
2015-11-23 06:47:50 +0900

    コントローラ生成とビューの自動生成、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  resources :customers
+  # get 'customers/index'
+
+  # get 'customers/new'
+
+  # get 'customers/create'
+
+  # get 'customers/edit'
+
+  # get 'customers/update'
+
+  # get 'customers/show'
+
+  # get 'customers/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
