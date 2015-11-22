a5c9bed462f3a819f90157f9e2397f77e9f2aed6
MR.Y <programming@yatsukouji.com>
2015-11-23 07:13:32 +0900

    独自メソッドに引数を渡す

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
-  def full_name
-    full_name = family_name + given_name
+  def full_name(sama)
+    full_name = family_name + given_name + sama
     return full_name
   end
 end
